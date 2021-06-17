7109c4c29db65c8a5c40f75ebf4cc8279c0435cb
Paweł Kobryński <pawelkobrynski97@gmail.com>
2021-06-17 19:07:24 +0200

    Dodanie requirements

--- main.py
+++ main.py
@@ -53,10 +53,8 @@ class User(db.Model):
 
 @auth.verify_password
 def verify_password(username_or_token, password):
-    # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
     if not user:
-        # try to authenticate with username/password
         user = User.query.filter_by(username=username_or_token).first()
         if not user or not user.verify_password(password):
             return False
